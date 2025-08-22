80d961a8753969307b35144c98ce1a3592abd3bf
lynx9687 <michal.manka@student.uj.edu.pl>
2025-08-22 11:46:42 +0200

    ActivePlayersMap tests.

---
+++ core/src/test/java/io/github/terraria/logic/players/ActivePlayersMapTest.java
@@ -0,0 +1,88 @@
+package io.github.terraria.logic.players;
+
+import com.badlogic.gdx.math.Vector2;
+import io.github.terraria.logic.IntRectangle;
+import io.github.terraria.logic.IntVector2;
+import io.github.terraria.logic.physics.Body;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivePlayersMapTest {
+    final ActivePlayersMap map = new ActivePlayersMap(new HashMap<>());
+    final List<PhysicalPlayer> physicalPlayers = new ArrayList<>();
+    final List<Vector2> positions = new ArrayList<>();
+    int n = 0;
+    Vector2 noPos;
+
+    Body createBody(Vector2 position) {
+        positions.add(position);
+        Body body = Mockito.mock(Body.class);
+        Mockito.when(body.getPosition()).thenReturn(position);
+        Mockito.when(body.liesOn(noPos)).thenReturn(false);
+        return body;
+    }
+
+    void createPhysicalPlayer(Vector2 position) {
+        PhysicalPlayer physicalPlayer = new PhysicalPlayer(new PlayerImpl(n, position), createBody(position));
+        physicalPlayers.add(physicalPlayer);
+        map.add(physicalPlayer);
+        n++;
+    }
+
+    @BeforeEach
+    void setUp() {
+        noPos = new Vector2(-1f, -1f);
+        createPhysicalPlayer(new Vector2(0f, 0f));
+        createPhysicalPlayer(new Vector2(0f, 0.5f));
+        createPhysicalPlayer(new Vector2(0.5f, 0.5f));
+        createPhysicalPlayer(new Vector2(0.5f, 1f));
+        createPhysicalPlayer(new Vector2(1f, 1f));
+        for(int i = 0; i < n; i++) {
+            Body body = physicalPlayers.get(i).body();
+            for(int j = 0; j < n; j++) {
+                Vector2 pos = positions.get(j);
+                Mockito.when(body.liesOn(pos)).thenReturn(i == j);
+            }
+        }
+    }
+
+    @Test
+    void getAtNoPos() {
+        assertNull(map.getAt(noPos));
+    }
+
+    @Test
+    void getAt() {
+        final int i = 0;
+        Vector2 pos = positions.get(i);
+        PhysicalPlayer phy = physicalPlayers.get(i);
+        assertEquals(phy, map.getAt(pos));
+    }
+
+    @Test
+    void getLocalLeftBottom() {
+        IntRectangle rectangle = new IntRectangle(0, 0, 2, 2);
+        assertThat(map.getLocal(rectangle).getList()).containsExactlyInAnyOrderElementsOf(physicalPlayers);
+    }
+
+    @Test
+    void getLocalRightTop() {
+        IntRectangle rectangle = new IntRectangle(0, 0, 1, 1);
+        assertThat(map.getLocal(rectangle).getList()).containsExactlyInAnyOrderElementsOf(physicalPlayers.subList(0, 3));
+    }
+
+    @Test
+    void getLocalDegenerate() {
+        IntVector2 pos = IntVector2.toInt(positions.get(0));
+        IntRectangle rectangle = new IntRectangle(pos, pos);
+        assertThat(map.getLocal(rectangle).getList()).isEmpty();
+    }
+}
