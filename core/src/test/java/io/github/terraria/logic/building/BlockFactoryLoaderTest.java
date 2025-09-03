65125b6c94f25c5e8ce9a2e6deea8a951c665f5e
lynx9687 <michal.manka@student.uj.edu.pl>
2025-09-03 15:25:37 +0200

    Test clean-up.

--- core/src/test/java/io/github/terraria/logic/building/BlockFactoryLoaderTest.java
+++ core/src/test/java/io/github/terraria/logic/building/BlockFactoryLoaderTest.java
@@ -7,6 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class BlockFactoryLoaderTest {
     @Test
     void loadingTest() {
+        Block block = new BlockFactoryLoader("testBlocks.json").getBlockFactory().create("Water");
+        assertEquals(new BlockType(3, "Water", false, 1), block.type());
+    }
+    @Test
+    void physicalLayerTest() {
         Block block = new BlockFactoryLoader("testBlocks.json").getBlockFactory().create("Dirt");
         assertEquals(new BlockType(2, "Dirt", true, 0), block.type());
     }
