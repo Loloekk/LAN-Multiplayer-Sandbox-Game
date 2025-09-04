60f7f86a5e8f426c84a887b52bb1a044a465b39b
Oleksandr Tymkovych <oleksandr.tymkovych@student.uj.edu.pl>
2025-09-04 03:25:41 +0200

    tests for recipe service

---
+++ core/src/test/java/io/github/terraria/logic/crafting/CraftingServiceTest.java
@@ -0,0 +1,81 @@
+package io.github.terraria.logic.crafting;
+
+import io.github.terraria.logic.ItemHolder;
+import io.github.terraria.logic.ItemRegistry;
+import io.github.terraria.logic.building.BlockFactory;
+import io.github.terraria.logic.building.BlockFactoryLoader;
+import io.github.terraria.logic.crafting.station.StationType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CraftingServiceTest {
+    private static final int cap = 10;
+    private static class DummyItemHolder extends ItemHolder {
+        public DummyItemHolder() {
+            super(cap);
+        }
+        public DummyItemHolder(int cap) {
+            super(cap);
+        }
+    }
+
+    private static ItemRegistry itemRegistry;
+    private static RecipeRepoImpl recipeRepo;
+    private ItemHolder itemHolder;
+    private CraftingService service;
+
+    @BeforeAll
+    static void setup() {
+        BlockFactory blockFactory = new BlockFactoryLoader("testBlocks.json").getBlockFactory();
+        itemRegistry = new ItemRegistry(blockFactory);
+        recipeRepo = new RecipeRepoImpl(itemRegistry);
+    }
+
+    @BeforeEach
+    void setupTest() {
+        itemHolder = new DummyItemHolder();
+        service = new CraftingService(recipeRepo);
+    }
+
+    @Test
+    void canCraftWhenAvailable() {
+        Recipe recipe = recipeRepo.getById(1);
+        itemHolder.insert(itemRegistry.create("Stone"), 1);
+        assertTrue(service.canCraft(recipe, itemHolder));
+    }
+
+    @Test
+    void cannotCraftWhenNotAvailable() {
+        Recipe recipe = recipeRepo.getById(1);
+        assertFalse(service.canCraft(recipe, itemHolder));
+    }
+
+    @Test
+    void getAvailableRecipes() {
+        List<Recipe> workbenchRecipes = service.getAvailableRecipes(StationType.WORKBENCH);
+        assertFalse(workbenchRecipes.isEmpty());
+        assertEquals(StationType.WORKBENCH, workbenchRecipes.get(0).getStation());
+    }
+
+    @Test
+    void successfulCraft() {
+        Recipe recipe = recipeRepo.getById(1);
+        itemHolder.insert(itemRegistry.create("Stone"), 1);
+        boolean crafted = service.craft(recipe, itemHolder);
+        assertTrue(crafted);
+        assertEquals(4, itemHolder.browse().count(itemRegistry.create("Stone")));
+    }
+
+    @Test
+    void failedCraft() {
+        Recipe recipe = recipeRepo.getById(1);
+        boolean crafted = service.craft(recipe, itemHolder);
+        assertFalse(crafted);
+        assertEquals(0, itemHolder.browse().count(itemRegistry.create("Stone")));
+    }
+}
