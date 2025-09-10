1dd113f9a7abd59cd22440c6cd2b9fa7636d35db
lynx9687 <michal.manka@student.uj.edu.pl>
2025-09-10 15:47:54 +0200

    Top boundary shift.

--- core/src/main/java/io/github/terraria/logic/physics/Box2DWorld.java
+++ core/src/main/java/io/github/terraria/logic/physics/Box2DWorld.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
+import io.github.terraria.logic.players.PlayerActivator;
 
 public class Box2DWorld implements World {
     private final com.badlogic.gdx.physics.box2d.World world;
@@ -33,17 +34,20 @@ public class Box2DWorld implements World {
 
         // Top wall
         shape.setAsBox(width * 0.5f, thickness * 0.5f,
-            new Vector2(left + width * 0.5f, bottom + height + thickness * 0.5f), 0f);
+            new Vector2(left + width * 0.5f,
+                bottom + height + thickness * 0.5f + PlayerActivator.MAX_PLAYERS_RADIUS * 2f), 0f);
         walls.createFixture(shape, 0f);
 
         // Left wall
-        shape.setAsBox(thickness * 0.5f, height * 0.5f,
-            new Vector2(left - thickness * 0.5f, bottom + height * 0.5f), 0f);
+        shape.setAsBox(thickness * 0.5f, height * 0.5f + PlayerActivator.MAX_PLAYERS_RADIUS,
+            new Vector2(left - thickness * 0.5f,
+                bottom + height * 0.5f + PlayerActivator.MAX_PLAYERS_RADIUS), 0f);
         walls.createFixture(shape, 0f);
 
         // Right wall
-        shape.setAsBox(thickness * 0.5f, height * 0.5f,
-            new Vector2(left + width + thickness * 0.5f, bottom + height * 0.5f), 0f);
+        shape.setAsBox(thickness * 0.5f, height * 0.5f + PlayerActivator.MAX_PLAYERS_RADIUS,
+            new Vector2(left + width + thickness * 0.5f,
+                bottom + height * 0.5f + PlayerActivator.MAX_PLAYERS_RADIUS), 0f);
         walls.createFixture(shape, 0f);
 
         shape.dispose();
