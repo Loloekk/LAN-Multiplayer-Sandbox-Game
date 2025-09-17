41cfe208844883db5d760d0525c8f345979ea4ec
Loloekk <karolbie@interia.pl>
2025-09-18 00:39:15 +0200

    right click implementation

--- core/src/main/java/io/github/terraria/client/PlayerInteractions.java
+++ core/src/main/java/io/github/terraria/client/PlayerInteractions.java
@@ -9,22 +9,22 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import com.esotericsoftware.kryonet.Connection;
 import io.github.terraria.client.state.ClientGameState;
 import io.github.terraria.common.Config;
-import io.github.terraria.controler.network.PacketPlayerClientToServer.PacketPlayerHit;
-import io.github.terraria.controler.network.PacketPlayerClientToServer.PacketPlayerMove;
+import io.github.terraria.controler.network.PacketClientToServer.PacketPlayerHit;
+import io.github.terraria.controler.network.PacketClientToServer.PacketPlayerMove;
+import io.github.terraria.controler.network.PacketClientToServer.PacketPlayerTouch;
 
 public class PlayerInteractions {
     Connection conn;
-    private int playerId;
     Viewport viewport;
     ClientGameState gameState;
 
     private long lastLeftClickTime = 0;
+    private long lastRightClickTime = 0;
     private static final long LEFT_CLICK_DELAY = Config.LEFT_CLICK_DELAY;
-    public PlayerInteractions(Connection conn, ClientGameState gameState, int playerId, Viewport viewport)
+    public PlayerInteractions(Connection conn, ClientGameState gameState, Viewport viewport)
     {
         this.conn = conn;
         this.gameState = gameState;
-        this.playerId = playerId;
         this.viewport = viewport;
     }
     public void handleInput() {
@@ -38,7 +38,7 @@ public class PlayerInteractions {
         if (Gdx.input.isKeyPressed(Input.Keys.W))    jump=true;
         if (mx!=0 || jump!=false) {
             PacketPlayerMove move = new PacketPlayerMove();
-            move.playerId = playerId;
+            move.playerId = gameState.getPlayerState().getPlayerId();
             move.moveX = mx;
             move.jump = jump;
             conn.sendUDP(move);
@@ -54,10 +54,27 @@ public class PlayerInteractions {
                 mousePos = gameState.getGamePosition(mousePos);
 //                System.out.println("Klik LPM w świecie gry: " + mousePos.x + ", " + mousePos.y);
                 PacketPlayerHit hit = new PacketPlayerHit();
-                hit.playerId = playerId;
+                hit.playerId = gameState.getPlayerState().getPlayerId();
                 hit.x = mousePos.x;
                 hit.y = mousePos.y;
-                conn.sendUDP(hit);
+                conn.sendTCP(hit);
+//                System.out.println("Send hiting " + playerId);
+            }
+        }
+        if (Gdx.input.isButtonPressed(Input.Buttons.RIGHT)) {
+            long now = TimeUtils.millis();
+            if (now - lastRightClickTime >= LEFT_CLICK_DELAY) {
+                lastRightClickTime = now;
+                Vector3 mousePos3 = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+                viewport.unproject(mousePos3);
+                Vector2 mousePos = new Vector2(mousePos3.x, mousePos3.y);
+                mousePos = gameState.getGamePosition(mousePos);
+//                System.out.println("Klik LPM w świecie gry: " + mousePos.x + ", " + mousePos.y);
+                PacketPlayerTouch touch = new PacketPlayerTouch();
+                touch.playerId = gameState.getPlayerState().getPlayerId();
+                touch.x = mousePos.x;
+                touch.y = mousePos.y;
+                conn.sendTCP(touch);
 //                System.out.println("Send hiting " + playerId);
             }
         }
